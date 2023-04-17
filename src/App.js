51efeb52740c7abd89d294f8237853f7f416610d
Keen Regen <keenregen@gmail.com>
2023-04-17 11:59:10 +0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <form
+            className="col-lg-5"
+            style={{
+              marginTop: "13px",
+              paddingRight: "15px",
+              paddingLeft: "15px",
+              paddingTop: "11px",
+              paddingBottom: "20px",
+              borderRadius: "33px",
+              boxShadow: "1px 1px 10px",
+            }}
+          >
+            <h4>Mint Portal</h4>
+            <h5>Use The Button To Connect Your Wallet</h5>
+            <Button style={{ marginBottom: "7px" }}>Connect Wallet</Button>
+            <div
+              className="card"
+              id="wallet-address"
+              style={{
+                marginTop: "7px",
+                paddingRight: "7px",
+                paddingLeft: "7px",
+                paddingTop: "7x",
+                paddingBottom: "7px",
+                borderRadius: "15px",
+                boxShadow: "1px 1px 10px",
+              }}
+            >
+              <label htmlFor="floatingInput">Wallet Address</label>
+              <label>Select the amount of NFTs to mint (min:1, max:3)</label>
+              <input
+                type="number"
+                name="amount"
+                defaultValue="1"
+                min="1"
+                max="3"
+              />
+              <Button style={{ marginTop: "3px", marginBottom: "3px" }}>
+                Mint/Buy
+              </Button>
+              <label>Price: 0.001 ETH per NFT.</label>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
